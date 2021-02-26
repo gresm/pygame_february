dcfc56e7472feedbcf39eb44e98bcd986f12843c
gresm <kwadrantek@gmail.com>
2021-02-26 19:34:31 +0100

    v 0.0.4

--- asserts/sourse/level.py
+++ asserts/sourse/level.py
@@ -1,19 +1,62 @@
-class Vector:
-    def __init__(self, x: int, y:int):
-        self.y = y
-        self.x = x
+from typing import Union, List
+
+
+class Vector2:
+    __slots__ = ("x", "y")
+    x: Union[float, int]
+    y: Union[float, int]
+
+    def __init__(self, x: Union[float, int], y: Union[float, int]):
+        object.__setattr__(self, "x", x)
+        object.__setattr__(self, "y", y)
+
+    def __setattr__(self, key, value):
+        raise AttributeError
+
+    def size(self) -> float:
+        return sum((self.x**2, self.y**2))**0.5
+
+    def __mul__(self, other: Union["Vector2", float, int]):
+        if isinstance(other, Vector2):
+            return Vector2(self.x * other.x, self.y * other.y)
+        else:
+            return Vector2(self.x * other, self.y * other)
+
+    def __truediv__(self, other: Union["Vector2", float, int]) -> "Vector2":
+        if isinstance(other, Vector2):
+            return Vector2(self.x / other.x, self.y / other.y)
+        else:
+            return Vector2(self.x / other, self.y / other)
+
+    def __add__(self, other: Union["Vector2", float, int]):
+        if isinstance(other, Vector2):
+            return Vector2(self.x + other.x, self.y + other.y)
+        else:
+            return Vector2(self.x + other, self.y + other)
+
+    def __sub__(self, other: Union["Vector2", float, int]):
+        if isinstance(other, Vector2):
+            return Vector2(self.x - other.x, self.y - other.y)
+        else:
+            return Vector2(self.x - other, self.y - other)
+
+    def __repr__(self):
+        return f"Vector2({self.x}, {self.y})"
+
+    def normalized(self) -> "Vector2":
+        return self / self.size()
 
 
 class Filed:
 
-    def __init__(self, pos: Vector, board: "Board"):
+    def __init__(self, pos: Vector2, board: "Board"):
         self.board = board
         self.pos = pos
 
 
 class Board:
-    def __init__(self, pos: Vector):
-        self.board = [[None for _ in range(pos.x)] for _ in range(pos.y)]
+    def __init__(self, pos: Vector2):
+        self.board: List[List] = [[None for _ in range(pos.x)] for _ in range(pos.y)]
 
-    def put(self, pos: Vector, filed: Filed):
+    def put(self, pos: Vector2, filed: Filed):
         self.board[pos.x][pos.y] = filed

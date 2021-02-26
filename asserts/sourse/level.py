0ab676fcadaa48b7c8651aec551a518cb9e92e35
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2021-02-26 18:15:56 +0100

    v 0.0.1

---
+++ asserts/sourse/level.py
@@ -0,0 +1,19 @@
+class Vector:
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+
+
+class Filed:
+
+    def __init__(self, pos: Vector, board: "Board"):
+        self.board = board
+        self.pos = pos
+
+
+class Board:
+    def __init__(self, pos: Vector):
+        self.board = [[None for _ in range(pos.x)] for _ in range(pos.y)]
+
+    def put(self, pos: Vector, filed: Filed):
+        self.board[pos.x][pos.y] = filed

120b324ffdc980fe7109215adf1e6e58f006bd31
gresm <kwadrantek@gmail.com>
2021-02-28 19:09:11 +0100

    v 0.0.7 ~ graphics_manager.py

--- asserts/graphics/graphics_manager.py
+++ asserts/graphics/graphics_manager.py
@@ -1,19 +1,19 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Optional
 from enum import Enum
 import pygame as p
 
 
-SPRITES = {
+SPRITES = (
     "spikes_floor", "spikes_left", "spikes_right", "spikes_ceiling", "spikes_floating", "wall_bottom",
     "wall_bottom_left", "wall_bottom_right", "wall_center", "wall_flat_top", "wall_flat_top_left_corner",
     "wall_flat_top_right_corner", "wall_floating", "wall_floating_both", "wall_floating_left",
     "wall_floating_right", "wall_left_n_right", "wall_open_left", "wall_open_right", "wall_top"
-}
+)
 
 
 class AnimatedSprite(p.sprite.Sprite):
     def __init__(self, frames: Union[List[p.Surface], Tuple[p.Surface]], max_ticks: int, x: int = 0, y: int = 0,
-                 *groups: p.sprite.AbstractGroup):
+                 hit_box: Optional[p.Rect] = None, *groups: p.sprite.AbstractGroup):
         super().__init__(*groups)
         self.y = y
         self.x = x
@@ -21,8 +21,12 @@ class AnimatedSprite(p.sprite.Sprite):
         self.frame = 0
         self.update_image()
         self.rect = self.image.get_rect()
+        self.hit_box = hit_box if hit_box else self.image.get_rect().copy()
         self.max_ticks = max_ticks
         self.tick_count = 0
+
+    def collide_with(self, other: "AnimatedSprite"):
+        return self.rect.colliderect(other.rect)
 
     def update_image(self):
         self.image = self.frames[self.frame]
@@ -61,9 +65,7 @@ class AnimatedSprite(p.sprite.Sprite):
 
 def get_sprite(name: str, ticks: int, x: int = 0, y: int = 0) -> AnimatedSprite:
     # noinspection PyProtectedMember
-    s = Sprites._get_sprite(name)
-    s.max_ticks = ticks
-    s.goto(x, y)
+    s = Sprites._get_sprite(name, ticks, x, y)
     return s
 
 
@@ -74,13 +76,14 @@ class Sprites(Enum):
         return object.__getattribute__(self, "_get_sprite")(item)
 
     @staticmethod
-    def _get_sprite(item: str) -> "AnimatedSprite":
+    def _get_sprite(item: str, max_ticks: int, x: int, y: int) -> "AnimatedSprite":
         if item in SPRITES:
             f = item + ".png"
             s = item + "_2.png"
             fd = item + "_3.png"
             ft = item + "_4.png"
-            return AnimatedSprite([p.image.load(f), p.image.load(s), p.image.load(fd), p.image.load(ft)], 1)
+            return AnimatedSprite([p.image.load(f), p.image.load(s), p.image.load(fd), p.image.load(ft)], max_ticks, x,
+                                  y)
         else:
             raise AttributeError
 
